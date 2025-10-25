85435bac0a80623bd41576f72f95c20b8ab1cb52
Kasimirus <ksmrusabmny@gmail.com>
2025-10-26 01:50:22 +0700

    Initial commit

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kategori;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $allkate = kategori::all();
+        return view('kategori.index', compact('allkate'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validasi
+        $validatedData = $request->validate([
+            'nama_kategori' => 'required'
+        ]);
+
+        kategori::crate($validatedData);
+
+        return Redirect()->route('kategori.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(kategori $kategori)
+    {
+        return view('kategori.show', compact('kategori'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(kategori $kategori)
+    {
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, kategori $kategori)
+    {
+        //validasi
+        $validatedData = $request->validate([
+            'nama_kategori' => 'required'
+        ]);
+
+        $kategori->update($validatedData);
+
+        return Redirect()->route('kategori.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(kategori $kategori)
+    {
+        $kategori->delete();
+        return Redirect()->route('kategori.index');
+    }
+}
---
+++ app/Http/Controllers/PenerbitController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\penerbit;
+use Illuminate\Http\Request;
+
+class PenerbitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, penerbit $penerbit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(penerbit $penerbit)
+    {
+        //
+    }
+}
